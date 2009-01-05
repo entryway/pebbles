27c7c1066656ec30e2499bc88290a3bb0b576149
bwilson <bw@b-ws-macbook.(none)>
2009-01-05 09:00:48 +0800

    cleaned up CategoriesController to REST with a little nicer parent category assignment, two CategoryImage methods still hangin out
    Signed-off-by: gustin <git@entryway.net>

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,151 +1,105 @@
-
 module Admin
-
-class CategoriesController < ApplicationController
-  layout "admin"
-  require_role "admin"
-  
-  # add an image to a category
-  def add_category_image
-    image = CategoryImage.new(params[:category_image])
-    category = Category.find(params[:id])
-    category.category_images << image
+  class CategoriesController < ApplicationController
+    layout "admin"
+    #  require_role "admin"
     
-    responds_to_parent do
-      render :update do |page| 
-        page[:image_list].replace_html :partial => 'image_list', :locals => { :category => category }
+    # add an image to a category
+    def add_category_image
+      image = CategoryImage.new(params[:category_image])
+      category = Category.find(params[:id])
+      category.category_images << image
+      
+      responds_to_parent do
+        render :update do |page| 
+          page[:image_list].replace_html :partial => 'image_list', :locals => { :category => category }
+        end
       end
-    end
-  end  
-  
-  # remove an image from the category
-  def remove_category_image
-    @category = Category.find(params[:id])
-    @image = CategoryImage.find(params[:image_id])
-    @category.category_images.delete(@image)  
-    
-    respond_to do |format|
-      format.js { 
-        render :partial => 'image_list', :locals => { :category => @category }
-      }
     end  
-  end
-  
-  # add new category
-  # TODO: this should be a POST/REST call, not a separate method?
-  def add_category
-    @categories = Category.root.all_children
-    @category = Category.find(:first)   
     
-    category = Category.new(:name => params[:category][:name],  
-                            :description => params[:category][:description],
-                            :active => params[:category][:active])
-    category.save!
-    parent = Category.find(params[:parent_category])
-    category.move_to_child_of parent
-    
-    render :nothing => true
-  end
-  
-  # setup to edit selectd category
-  def setup_edit_category
-    categories = Category.root
-    category = Category.find(params[:category_selector])
-    
-    render :partial => 'edit', :locals => { :categories => categories, :category => category }
-  end
-  
-  # edit submitted category
-  def edit_category
-    category = Category.find(params[:id])
-    category.name = params[:category][:name]
-    category.save!
-    parent = Category.find(params[:parent_category])
-    category.move_to_child_of parent  
-	
-	categories = Category.root
-	
-    render :partial => 'edit', :locals => { :categories => categories, :category => category }
-  end
-  
-
-
-  # the REST
-  
-  # GET /categories
-  # GET /categories.xml
-  def index
-    @categories = Category.root.all_children
-    
-    respond_to do |format|
-      format.html # index.rhtml
-      format.xml  { render :xml => @categories.to_xml }
+    # remove an image from the category
+    def remove_category_image
+      @category = Category.find(params[:id])
+      @image = CategoryImage.find(params[:image_id])
+      @category.category_images.delete(@image)  
+      
+      respond_to do |format|
+        format.js { 
+          render :partial => 'image_list', :locals => { :category => @category }
+        }
+      end  
     end
-  end
+    
+    def create
+      @category = Category.new(params[:category])
+      @category.position = Category
+      
+      respond_to do |format|
+        if @category.save
 
-  # GET /categories/1
-  # GET /categories/1.xml
-  def show
-    @category = Category.find(params[:id])
+          parent_category = Category.find(params[:category][:parent_id])
+          @category.move_to_child_of parent_category
 
-    respond_to do |format|
-      format.html # show.rhtml
-      format.xml  { render :xml => @category.to_xml }
-    end
-  end
-
-  # GET /categories/new
-  def new
-    @category = Category.new
-  end
-
-  # GET /categories/1;edit
-  def edit
-    @category = Category.find(params[:id])
-  end
-
-  # POST /categories
-  # POST /categories.xml
-  def create
-    @category = Category.new(params[:category])
-
-    respond_to do |format|
-      if @category.save
-        flash[:notice] = 'Category was successfully created.'
-        format.xml  { head :created, :location => category_url(@category) }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @category.errors.to_xml }
+          flash[:notice] = 'Category was successfully created.'
+          format.html { redirect_to(@category) }
+        else
+          format.html { render :action => "new" }
+        end
       end
     end
-  end
+    
+    def edit
+      @category = Category.find(params[:id])
+    end
+    
+    def update
+      @category = Category.find(params[:id])
 
-  # PUT /categories/1
-  # PUT /categories/1.xml
-  def update
-    @category = Category.find(params[:id])
+      respond_to do |format|
+        if @category.update_attributes(params[:category])
 
-    respond_to do |format|
-      if @category.update_attributes(params[:category])
-        flash[:notice] = 'Category was successfully updated.'
-        format.html { redirect_to category_url(@category) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @category.errors.to_xml }
+          parent = Category.find(params[:category][:parent_id])
+          @category.move_to_child_of parent
+
+          flash[:notice] = 'Category was successfully updated.'
+          format.html { redirect_to(@category) }
+        else
+          format.html { render :action => "edit" }
+        end
       end
     end
-  end
+    
+    def index
+      @categories = Category.find(:all, :order => 'lft ASC')
+      
+      respond_to do |format|
+        format.html # index.rhtml
+      end
+    end
 
-  # DELETE /categories/1
-  # DELETE /categories/1.xml
-  def destroy
-    category = Category.find(params[:id])
-    category.destroy
-	@categories = Category.root.all_children
-	
-	render :action => 'index'
-  end
-end
+    def show
+      @category = Category.find(params[:id])
 
+      respond_to do |format|
+        format.html # show.rhtml
+      end
+    end
+
+    def new
+      @category = Category.new
+    end
+
+    def edit
+      @category = Category.find(params[:id])
+    end
+
+    def destroy
+      category = Category.find(params[:id])
+      category.destroy
+      
+      respond_to do |format|
+        format.html { redirect_to(categories_url) }
+      end
+    end
+
+  end
 end
