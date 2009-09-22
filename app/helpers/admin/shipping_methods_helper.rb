ac13624b4dad2f48dc3468b4916318d3ba6b155a
bobbyw <bobbywilson0@gmail.com>
2009-09-22 14:58:45 -0600

    removed old helpers

--- app/helpers/admin/shipping_methods_helper.rb
+++
@@ -1,9 +0,0 @@
-module Admin::ShippingMethodsHelper
-	def add_fulfillment_link(name)
-	  link_to_function name do |page|
-			page.insert_html :bottom, :fulfillment_codes, 
-							         :partial => 'fulfillment_code',
-							         :object => FulfillmentCode.new
-		end
-	end
-end
--- app/helpers/stores_helper.rb
+++
@@ -1,6 +0,0 @@
-module StoresHelper
-  
-  def set_decimal(number)
-    number = sprintf("%.2f", number)
-  end
-end
