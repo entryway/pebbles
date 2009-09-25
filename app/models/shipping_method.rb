35f6cd8630d6317c5a0ffd21acfeeaeaac0bf0fc
bobbyw <bobbywilson0@gmail.com>
2009-09-25 14:17:22 -0600

    added shipping method spec for flat rate

--- app/models/shipping_method.rb
+++ app/models/shipping_method.rb
@@ -8,9 +8,17 @@ class ShippingMethod < ActiveRecord::Base
   
   accepts_nested_attributes_for :flat_rate_shippings, :allow_destroy => true,
                                 :reject_if => proc{ |attributes| attributes['flat_rate'].blank? }
-                                
+
+  ##
+  # returns the shipping rate of the flat_rate_shippings with the highest order_total_low
+  # that is less than or equal to the order_total for the shipping method
+  #
+  # @param[Float] total order_total
+  # @return[Float] flat_rate of the flat_rate_shipping
   def flat_rate_by_order_total(total)
-    self.flat_rate_shippings.ordered_by_total_ranges
+    shipping = self.flat_rate_shippings.ordered_by_total_ranges.find(:last, 
+                                                                     :conditions => "order_total_low <= #{total}")
+    shipping.flat_rate
   end
     
     
--- spec/models/shipping_method_spec.rb
+++ spec/models/shipping_method_spec.rb
@@ -6,12 +6,12 @@ describe ShippingMethod do
     before(:each) do
       @shipping_method = Factory(:shipping_method)
       {0 => 2.99, 10.00 => 3.99, 20.00 => 4.99}.each do |k, v|
-        @shipping_method.flat_rate_shippings.create(:flat_rate => k, :order_total_low => v)
+        @shipping_method.flat_rate_shippings.create(:order_total_low => k, :flat_rate => v)
       end
     end
     
     it "should return the correct flat_rate shipping for an order total" do
-      {0 => 2.99, 9.99 => 2.99, 10 => 3.99, 15 => 3.99, 25 => 4.99}.each do |k, v|
+      {0 => 2.99, 9.99 => 2.99, 10 => 3.99, 15 => 3.99, 20 => 4.99, 25 => 4.99}.each do |k, v|
         @shipping_method.flat_rate_by_order_total(k).should == v
       end
     end
