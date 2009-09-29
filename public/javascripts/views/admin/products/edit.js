ea0b8ab785891d204f06726d2020c18d3932f2c9
jonathangreenberg <greenberg@entryway.net>
2009-09-29 00:17:48 -0400

    getting set up for product_image_thumbnails

--- public/javascripts/views/admin/products/edit.js
+++ public/javascripts/views/admin/products/edit.js
@@ -10,8 +10,18 @@ jQuery(document).ready(function() {
     jQuery(this).ajaxSubmit(product_image_options); 
     return false; 
   });
+  jQuery('#image_thumbnail_add').submit(function() { 
+    jQuery(this).ajaxSubmit(product_image_options); 
+    return false; 
+  });
   jQuery(".destroy-product-image").live("click", function() {
     if (confirm( "Are you sure you want to remove the image?")) {
+      jQuery("#image_list").load(jQuery(this).attr('href'), { _method: "DELETE" });
+    }
+    return false;
+  })
+  jQuery(".destroy-product-image-thumbnail").live("click", function() {
+    if (confirm( "Are you sure you want to remove the thumbnail?")) {
       jQuery("#image_list").load(jQuery(this).attr('href'), { _method: "DELETE" });
     }
     return false;
