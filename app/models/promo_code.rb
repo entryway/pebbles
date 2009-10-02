1904f69db2606f8a9dee445e539c05fc01195d0e
jonathangreenberg <greenberg@entryway.net>
2009-10-02 16:50:21 -0400

    green-label promocode update

--- app/models/promo_code.rb
+++ app/models/promo_code.rb
@@ -7,50 +7,64 @@ class PromoCode < ActiveRecord::Base
   
   SUCCESS_MESSAGE = "Promo Code successfully applied, enjoy."
   NOT_APPLIED_MESSAGE = "Promo Code not applied."
+  MINIMUM_AMOUNT_MESSAGE = "Your order did not reach the minimum amount required of "
   INVALID_MESSAGE = "This is not a valid promo code, please check and reapply."
-  
+ 
+  # apply a specific promo code to an order
+  def self.apply(order, promo_code)
+    begin
+      promo_code = promo_code.upcase 
+      promo = PromoCode.find_by_code(promo_code)
+    rescue
+      order.promo_code = ''
+      order.free_shipping = false
+      order.promo_discount = ''
+      order.save!
+    end 
+    if promo
+      promo.apply(order)
+    else
+      promo = PromoCode.new
+      promo.message = INVALID_MESSAGE
+    end
+     
+    promo
+  end
+   
   # apply the promo code to a particular order
   def apply(order)
     self.message = INVALID_MESSAGE
     
-    if self.percent_discount && self.percent_discount > 0
-      discount = (self.percent_discount / 100) * 
-                  order.sub_total
-    else
-      discount = self.dollar_discount          
-    end
-    self.message = SUCCESS_MESSAGE
+    if order.sub_total >= self.minimum_order_amount
+      # order is the minimum amount
+      if self.percent_discount && self.percent_discount > 0
+        discount = self.calculate_percent_discount(order)
+      else
+        discount = self.dollar_discount          
+      end
+      discount ||= 0
+      self.message = SUCCESS_MESSAGE
     
-    if order
-      order.promo_code = self.code.upcase
-      order.free_shipping = true if self.free_shipping
-      order.promo_discount = discount
-      order.save!
+      if order
+        order.promo_code = self.code.upcase
+        order.free_shipping = true if self.free_shipping
+        order.promo_discount = discount
+        order.save!
+      end
+    else
+      self.message = minimum_amount_required_message
     end
     
     self.message
   end
+  
+  def calculate_percent_discount(order)
+    (self.percent_discount / 100) * order.sub_total
+  end
    
-   
-   # promo code application
-   def self.apply(order, promo_code)
-     begin
-       promo_code = promo_code.upcase 
-       promo = PromoCode.find_by_code(promo_code)
-     rescue
-       order.promo_code = ''
-       order.free_shipping = false
-       order.promo_discount = ''
-       order.save!
-     end 
-     if promo
-       promo.apply(order)
-     else
-       promo = PromoCode.new
-       promo.message = INVALID_MESSAGE
-     end
-     
-     promo
-   end
+  def minimum_amount_required_message
+    MINIMUM_AMOUNT_MESSAGE + '$' + self.minimum_order_amount.to_s
+  end
+  
    
 end
---
+++ spec/models/promo_code_spec.rb
@@ -0,0 +1,56 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+
+describe 'Promo codes ' do
+
+  before(:each) do
+    @promo_code = Factory(:promo_code)
+   # @order = Factory(:order)
+    @order = Order.new
+    @order.product_cost = 0
+    @order.shipping_cost = 0
+    @order.email = 'blah@blah.com'
+    @order.drop_shipping_cost = 0
+    @order.tax = 0
+    @order.full_name = 'fred smith'
+    @order.order_type = '1'
+    @order.promo_discount = 0
+  end
+  
+  it 'applies to orders exceeding the minimum amount required' do
+    @promo_code.minimum_order_amount = 60.00
+    @order.sub_total = 60.00
+    message = @promo_code.apply(@order)
+    message.should == PromoCode::SUCCESS_MESSAGE
+  end
+  
+  it 'fails to apply to orders not reaching the minimum amount required' do
+    @promo_code.minimum_order_amount = 60.00
+    @order.product_cost = 59.00
+    message = @promo_code.apply(@order)
+    message.should == @promo_code.minimum_amount_required_message
+  end
+  
+  it 'activates free shipping' do
+    @promo_code.free_shipping = true
+    @order.product_cost = 59.00
+    @order.free_shipping = false
+    
+    message = @promo_code.apply(@order)
+    @order.free_shipping.should == true
+    message.should == PromoCode::SUCCESS_MESSAGE
+  end
+  
+  it 'applies dollar discount' do
+    @promo_code.dollar_discount = 5.00
+    @order.product_cost = 55.00
+    
+    message = @promo_code.apply(@order)
+    @order.total.should == 50.00
+    message.should == PromoCode::SUCCESS_MESSAGE
+  end
+  
+  it 'applies percentage discount' 
+  
+end
+
