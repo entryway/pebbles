0988a01225cc7ca98f2e2ed3a8430ed5f65a8279
bobbyw <bobbywilson0@gmail.com>
2009-10-06 20:08:02 -0600

    categories view

--- app/helpers/pebbles/category_helper.rb
+++ app/helpers/pebbles/category_helper.rb
@@ -11,5 +11,13 @@ module Pebbles::CategoryHelper
     image_tag("icon-#{category.name.split.first.downcase}.gif", :alt => 'icon')
   end
 
+  def category_li(category)
+    if category.leaf?
+      "<li class='leaf category_item' id='#{category.id}'>"
+    else
+      "<li class='branch category_item' id='#{category.id}'>"
+    end
+  end
+
 end
 
