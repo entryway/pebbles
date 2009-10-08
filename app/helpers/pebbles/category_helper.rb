0b52b71b2a63a562b5a5a30efadb5bb002001dae
jonathangreenberg <greenberg@entryway.net>
2009-10-08 14:15:13 -0400

    switching front end category images to icons

--- app/helpers/pebbles/category_helper.rb
+++ app/helpers/pebbles/category_helper.rb
@@ -10,6 +10,10 @@ module Pebbles::CategoryHelper
   def category_image(category)
     image_tag(category.category_images[0].filename.url, :alt => 'icon')
   end
+  
+  def category_icon(category)
+     image_tag(category.category_icon.filename.url, :alt => 'icon')
+   end
 
   def category_li(category)
     if category.leaf?
