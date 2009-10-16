ec149eb74efc0b349b856d59bed57bfb6547ff36
jonathangreenberg <greenberg@entryway.net>
2009-10-16 00:00:17 -0400

    hooking up packing receipts in order show to jQuery

--- public/javascripts/views/admin/orders/show.js
+++ public/javascripts/views/admin/orders/show.js
@@ -1,32 +1,26 @@
-Event.addBehavior({
-  	'#print-packing-sheet:click': function(e) {
-		$('logo').show();
-		window.print();
-		$('logo').hide();
-		return false;
-	},
-	'#print-gift-receipt:click': function(e) {
-		$('logo').show();
-		$('price-header').hide();
-		$('total-header').hide();
-		$$('td.price-value').each(function(element) {
-      		element.hide();
-      	});
-		$$('td.total-value').each(function(element) {
-      		element.hide();
-      	});
-		$('totals').hide();
-		window.print();
-		$('price-header').show();
-		$('total-header').show();
-		$$('td.price-value').each(function(element) {
-      		element.show();
-      	});
-		$$('td.total-value').each(function(element) {
-      		element.show();
-      	});
-		$('totals').show();
-		$('logo').hide();
-		return false;
-	}
-})
+(function($) { 
+  $(document).ready(function(){
+    $('#print-packing-sheet').click( function() {
+  		$('#logo').show();
+  		window.print();
+  		$('#logo').hide();
+  		return false;
+    });
+    $('#print-gift-receipt').click( function() {
+      $('#logo').show();
+  		$('#price-header').hide();
+  		$('#total-header').hide();
+  		$('.price-value').hide();
+  		$('.total-value').hide();
+  		$('#totals').hide();
+  		window.print();
+  		$('#price-header').show();
+  		$('#total-header').show();
+  		$('.price-value').show();
+  		$('.total-value').show();
+  		$('#totals').show();
+  		$('#logo').hide();
+  		return false;
+  	});
+  });
+})(jQuery);
