f7ad8cab86659fc6d01b74e7dcb65b5d895b9481
gustin <gustin@entryway.net>
2009-12-13 23:03:07 -0500

    - display an out of stock message if the product has been deleted from   the system - removal of hard tabs

--- app/helpers/pebbles/admin/orders_helper.rb
+++ app/helpers/pebbles/admin/orders_helper.rb
@@ -6,6 +6,16 @@ module Pebbles::Admin
       true if order.authorized?
     end
     
+
+    ##
+    # Displays an out of stock message if the product does not exist (deleted
+    # from the system) or is out of stock.
+    #
+    # @param [Product] product The product to determine if out of stock or not.
+    # @return [String] An out of stock message if product is so.
+    def out_of_stock_message(product)
+      "out of stock" unless product 
+    end
   end
   
   
