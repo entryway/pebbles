64423b304a3cb6a395e8a1166eb206e7466a0503
jonathangreenberg <greenberg@entryway.net>
2010-05-11 15:26:47 -0400

    changing pageless js call to use product_items partial

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,9 +14,12 @@ class CategoriesController < ApplicationController
     @products = @category.paged_products(params[:page], 15)
     respond_to do |format|
       format.html
-      @products = @category.descended_paged_products(params[:page], 9)
-      format.js { render(:partial =>  '/products/shirt_items', 
-                         :locals => { :products => @products }) }
+      format.js do
+        products = @category.descended_paged_products(params[:page], 9)
+        render :partial =>  '/products/product_items', 
+               :locals => { :products => products }
+        
+      end
     end
   end
 
