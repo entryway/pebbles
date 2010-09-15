129cec8a42f97b1f78042d9e6c8b8ef49effe36d
Ryan R. Smith <this.ryansmith@gmail.com>
2010-09-15 10:28:55 -0700

    Added a product factory

--- spec/factories/product_option.rb
+++ spec/factories/product_option.rb
@@ -1,6 +1,19 @@
-
 Factory.define :product_option do |p|
   p.name                    'size'
   p.selection_type          1
   p.description             'option description'
+end
+
+Factory.define :product do |f|
+  f.sku                { Factory.next(:sku) } 
+  f.name               'product1'
+  f.description        'description for product 1'
+  f.weight             1
+  f.length             20
+  f.width              10
+  f.height             10
+  f.shipping_type      ShippingType::DEFAULT_SHIPPING
+  f.available          true
+  f.price              5.50
+  f.admin_notes        'adminnotes'
 end
