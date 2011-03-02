ea4dc397cb8e4bcf7c7fa9d29a417aa0e66d9dcd
gustin <gustin@entryway.net>
2011-03-02 12:39:51 -0500

    [Finished #10648191] Customer views unavailable product extra check to make sure the product is available before adding so cached pages by google fail

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,27 +2,31 @@ class CartItemsController < ApplicationController
 
 
   def index
-  end 
+  end
 
 
   def new
     @cart_item = CartItem.new
   end
-  
+
   def create
     @cart = current_cart
     product_id = params[:id]
     quantity = params[:quantity]
     options = params[:options].map{|id| id.to_i} if params[:options]
-    @cart.add_product(product_id, quantity, options)
+    # ensure the product is available
+    product = Product.find(product_id)
+    if product.available
+      @cart.add_product(product_id, quantity, options)
+    end
     redirect_to cart_path(@cart)
-    
+
   #rescue
   #  flash[:message] = "Product not found"
   #  redirect_to '/'
   end
-  
-  
+
+
   def destroy
     cart = current_cart
     cart_item = cart.cart_items.find(params[:id])
@@ -31,8 +35,8 @@ class CartItemsController < ApplicationController
     end
     redirect_to cart_path(cart)
   end
-  
-  
- 
-  
+
+
+
+
 end
